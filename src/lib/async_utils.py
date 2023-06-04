e3203efb7da5c23a17680b2da031b992542a49d9
Lorenzo Paderi <lorenzo@mijorus.it>
2023-06-04 18:42:13 +0200

    added debouncer util

--- src/lib/async_utils.py
+++ src/lib/async_utils.py
@@ -21,3 +21,28 @@ def idle(func):
     def wrapper(*args, **kwargs):
         GLib.idle_add(func, *args)
     return wrapper
+
+
+def debounce(wait_time):
+    """
+    Decorator that will debounce a function so that it is called after wait_time seconds
+    If it is called multiple times, will wait for the last call to be debounced and run only this one.
+    See the test_debounce.py file for examples
+    """
+
+    def decorator(function):
+        def debounced(*args, **kwargs):
+            def call_function():
+                debounced._timer = None
+                return function(*args, **kwargs)
+
+            if debounced._timer is not None:
+                debounced._timer.cancel()
+
+            debounced._timer = threading.Timer(wait_time, call_function)
+            debounced._timer.start()
+
+        debounced._timer = None
+        return debounced
+
+    return decorator
