14064303de5d447ea56300ed11b7bc78eb223bbd
Lorenzo Paderi <lorenzo@mijorus.it>
2024-06-25 18:05:01 +0200

    wip

--- src/models/UpdateManager.py
+++ src/models/UpdateManager.py
@@ -14,7 +14,7 @@ from ..lib.utils import get_random_string
 from ..providers.AppImageProvider import AppImageProvider, AppImageListElement
 
 class UpdateManager(ABC):
-    @abstractmethod    
+    @abstractmethod
     def __init__(self, url: str) -> None:
         self.download_folder = GLib.get_tmp_dir() + '/it.mijorus.gearlever/downloads'
         pass
@@ -50,21 +50,28 @@ class UpdateManagerChecker():
 
         return None
 
-    def check_app(el: AppImageListElement):
+    def check_app(el: AppImageListElement) -> Optional[str]:
         if not terminal.host_sh(['which', 'readelf']):
             return
 
-        # Github url
         readelf_out = terminal.host_sh(['readelf', '--string-dump=.upd_info', '--wide', el.file_path])
         readelf_out = readelf_out.replace('\n', ' ')
-        pattern_gh = r"gh-releases-zsync\|(.*)$"
-        pattern_link = r"gh-releases-zsync\|(.*)$"
 
-        matches = re.match(pattern_gh, readelf_out) or \
-            re.match(pattern_link, readelf_out)
+        # Github url
+        pattern_gh = r"^gh-releases-zsync\|(.*)$"
+        matches = re.match(pattern_gh, readelf_out)
 
         if matches:
             return matches[0]
+        
+        # Static url
+        pattern_link = r"^zsync\|http(.*)$"
+        matches = re.match(pattern_link, readelf_out)
+
+        if matches:
+            return re.sub(r"^zsync\|", '', matches[0])
+
+        return None
 
 
 
@@ -141,9 +148,8 @@ class StaticFileUpdater(UpdateManager):
         app_conf = read_config_for_app(el)
         etag = resp.headers.get('etag', None)
 
-        if etag:
-            if 'last_update_hash' in app_conf:
-                is_etag_different = app_conf['last_update_hash'] == etag
+        if etag and 'last_update_hash' in app_conf:
+            is_etag_different = app_conf['last_update_hash'] == etag
 
         return is_size_different or is_etag_different
 
@@ -210,20 +216,69 @@ class GithubUpdater(UpdateManager):
     def cleanup(self):
         shutil.rmtree(self.download_folder)
 
+    def convert_glob_to_regex(glob_str):
+        """
+        Converts a string with glob patterns to a regular expression.
+
+        Args:
+            glob_str: A string containing glob patterns.
+
+        Returns:
+            A regular expression string equivalent to the glob patterns.
+        """
+        regex = ""
+        for char in glob_str:
+            if char == "*":
+                regex += r".*"
+            else:
+                regex += re.escape(char)
+
+        return regex
+
     def is_update_available(self, el: AppImageListElement):
-        resp = requests.head(self.url, allow_redirects=True)
-        resp_cl = int(resp.headers.get('content-length'))
-        old_size = os.path.getsize(el.file_path)
+        rel_url = f'https://api.github.com/repos/{self.url_data["username"]}'
+        rel_url += f'/{self.url_data["username"]}/releases/{self.url_data["release"]}'
 
-        logging.debug(f'StaticFileUpdater: new url has length {resp_cl}, old was {old_size}')
-        is_size_different = resp_cl != old_size
-        is_etag_different = False
+        try:
+            rel_data = requests.get(rel_url)
+        except Exception as e:
+            logging.error(e)
+            return False
+        
+        zsync_file = None
+        target_re = re.compile(self.convert_glob_to_regex(self.url_data['filename']))
+        for asset in rel_data['assets']:
+            if re.match(target_re, asset['name']) and asset['name'].endswith('.zsync'):
+                zsync_file = asset
+                break
 
-        app_conf = read_config_for_app(el)
-        etag = resp.headers.get('etag', None)
+        target_file = None
+        if zsync_file:
+            target_file = re.sub(r'\.zsync$', '', zsync_file['name'])
 
-        if etag:
-            if 'last_update_hash' in app_conf:
-                is_etag_different = app_conf['last_update_hash'] == etag
+        target_asset = None
+        if target_file:
+            for asset in rel_data['assets']:
+                if asset['name'] == target_file:
+                    target_asset = asset
+                    break
 
-        return is_size_different or is_etag_different
+        if target_asset:
+            ct_supported = asset['content_type'] in [*AppImageProvider.supported_mimes, 
+                                                     'binary/octet-stream', 'application/octet-stream']
+
+            if ct_supported:
+                app_conf = read_config_for_app(el)
+                old_size = os.path.getsize(el.file_path)
+                is_size_different = asset['size'] != old_size
+
+                if 'last_update_hash' in app_conf:
+                    is_id_different = app_conf['last_update_hash'] != asset['id']
+                    return is_id_different
+                else:
+                    return is_size_different
+
+        return False
+
+
+
