bf926b193364de54a7a3af5a8d88a65a15b130c1
Lorenzo Paderi <lorenzo@mijorus.it>
2025-01-27 17:42:40 +0100

    wip cli

--- src/Cli.py
+++ src/Cli.py
@@ -5,6 +5,7 @@ from gi.repository import Gtk, Gio, Adw, Gdk, GLib, GObject # noqa
 from .BackgroudUpdatesFetcher import BackgroudUpdatesFetcher
 from .lib.costants import FETCH_UPDATES_ARG
 from .providers.providers_list import appimage_provider
+from .providers.AppImageProvider import AppImageUpdateLogic
 from .lib.json_config import read_config_for_app, read_json_config
 from .models.UpdateManager import UpdateManagerChecker
 
@@ -17,6 +18,14 @@ class Cli():
         ('list-updates', 'List app updates'),
     ]
 
+    def ask(message: str, options: list) -> str:
+        _input = None
+
+        while not _input in options:
+            _input = input(message)
+
+        return _input
+
     def from_options(options):
         for opt in Cli.options:
             if options.contains(opt[0]):
@@ -28,7 +37,6 @@ class Cli():
 
     def get_file_from_args(options):
         args = sys.argv[1:]
-        file_path = None
 
         for a in args:
             if not a.startswith('-'):
@@ -39,24 +47,55 @@ class Cli():
 
         return None
 
+    def get_list_element_from_gfile(g_file: Gio.File):
+        el = None
+
+        for a in appimage_provider.list_installed():
+            if a.file_path == g_file.get_path():
+                el = a
+                break
+
+        if not el:
+            raise Exception('File not installed')
+    
+        return el
+
     def fetch_updates(options):
         BackgroudUpdatesFetcher.fetch()
+
+    def update(options):
+        g_file = Cli.get_file_from_args(options)
+        el = Cli.get_list_element_from_gfile(g_file)
+
+        app_conf = read_config_for_app(el)
+        update_url = app_conf.get('update_url', None)
+        manager = UpdateManagerChecker.check_url(update_url, el)
+
+        if not manager:
+            print('No update method was found for this app')
+            sys.exit(0)
+
+        print(f'Downloading update from:\n{manager.url}')
+        appimage_provider.update_from_url(manager, el, 
+            lambda s: print("\rStatus: " + round(s) + "%", end=""))
+
+        # print('Update completed')
 
     def uninstall(options):
         g_file = Cli.get_file_from_args(options)
         force = ('--delete' in sys.argv)
 
-        if not file_path:
+        if not g_file:
             sys.exit(1)
 
-        apps = appimage_provider.list_installed()
-        el = appimage_provider.create_list_element_from_file(g_file)
+        el = Cli.get_list_element_from_gfile(g_file)
         appimage_provider.uninstall(el, force_delete=force)
+        # print('Removed sucessfully')
 
     def integrate(options):
         g_file = Cli.get_file_from_args(options)
 
-        if not file_path:
+        if not g_file:
             sys.exit(1)
 
         el = appimage_provider.create_list_element_from_file(g_file)
@@ -65,6 +104,14 @@ class Cli():
         if not appimage_provider.can_install_file(g_file):
             print('This file format is not supported!')
             sys.exit(1)
+
+        apps = appimage_provider.list_installed()
+
+        already_installed = False
+        for a in apps:
+            if a.name == el.name:
+                already_installed = True
+                break
 
         if options.contains('--yes') == False \
             and options.contains('-y') == False:
@@ -80,9 +127,24 @@ class Cli():
             ans = input('\nDo you really want to integrate this AppImage? (y/N) ')
 
             if ans.lower() != 'y':
-                return
+                sys.exit(0)
+
+            if already_installed:
+                ans = Cli.ask('\nAnother version of this app is already integrated. How do you want to proceed? ((K)eep both/(R)eplace) ', 
+                    ['k', 'r', 'K', 'R'])
+
+            if ans.lower() == 'k':
+                el.update_logic = AppImageUpdateLogic.KEEP
+            elif ans.lower() == 'r':
+                el.update_logic = AppImageUpdateLogic.REPLACE
+        else:
+            el.update_logic = AppImageUpdateLogic.KEEP
+            
+            if options.contains('--replace'):
+                el.update_logic = AppImageUpdateLogic.REPLACE
 
         appimage_provider.install_file(el)
+        print(f'{el.name} was integrated successfully')
 
     def list_installed(options):
         apps = appimage_provider.list_installed()
