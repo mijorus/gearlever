ad7643fc3508b0158a19fa904088e4b6814b1882
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2025-04-13 14:25:44 +0200

    test workflow

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+import unittest
+import subprocess
+
+class TestGearLever(unittest.TestCase):
+    def runCommand(self, command: list[str]):
+        output = subprocess.run(['flatpak', 'run', 'it.mijorus.gearlever', *command])
+        output.check_returncode()
+        output_str = output.stdout.decode()
+        
+        print(output_str)
+        return output_str
+        
+    def test_list_installed(self):
+        self.runCommand(['--list-installed'])
