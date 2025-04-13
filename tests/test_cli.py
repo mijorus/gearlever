8396608d36559d8972adc24519c0cec2040a516c
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2025-04-13 14:39:17 +0200

    wip

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ import subprocess
 
 class TestGearLever(unittest.TestCase):
     def runCommand(self, command: list[str]):
-        output = subprocess.run(['flatpak', 'run', 'it.mijorus.gearlever', *command])
+        output = subprocess.run(['flatpak', 'run', 'it.mijorus.gearlever', *command], stdout=subprocess.PIPE)
         output.check_returncode()
         output_str = output.stdout.decode()
         
