4cc83958c1be773ba6853c01bb3f63d952b09c63
Lorenzo Paderi <lorenzo@mijorus.it>
2025-04-18 12:24:14 +0200

    wip codeberg

--- src/models/UpdateManager.py
+++ src/models/UpdateManager.py
@@ -17,6 +17,9 @@ from .Models import DownloadInterruptedException
 
 class UpdateManager(ABC):
     name = ''
+    system_arch = terminal.sandbox_sh(['arch'])
+    is_x86 = re.compile(r'(\-|\_|\.)x86(\-|\_|\.)')
+    is_arm = re.compile(r'(\-|\_|\.)(arm64|aarch64|armv7l)(\-|\_|\.)')
 
     @abstractmethod
     def __init__(self, url: str, embedded=False) -> None:
@@ -375,13 +378,9 @@ class GithubUpdater(UpdateManager):
                 logging.info(' - ' + t['name'])
 
             # Check possible differences with system architecture in file name
-            system_arch = terminal.sandbox_sh(['arch'])
-            is_x86 = re.compile(r'(\-|\_|\.)x86(\-|\_|\.)')
-            is_arm = re.compile(r'(\-|\_|\.)(arm64|aarch64|armv7l)(\-|\_|\.)')
-
-            if system_arch == 'x86_64':
+            if self.system_arch == 'x86_64':
                 for t in possible_targets:
-                    if is_x86.search(t['name']) or not is_arm.search(t['name']):
+                    if self.is_x86.search(t['name']) or not self.is_arm.search(t['name']):
                         zsync_file = t
                         logging.info('found possible target: ' + t['name'])
                         break
@@ -445,7 +444,7 @@ class GitlabUpdater(UpdateManager):
     def get_url_data(url: str):
         paths = []
         if url.startswith('https://'):
-            logging.debug(f'GitlabUpdater: found http url, trying to detect github data')
+            logging.debug(f'GitlabUpdater: found http url, trying to detect gitlab data')
             urldata = urlsplit(url)
 
             if urldata.netloc != 'gitlab.com':
@@ -465,7 +464,7 @@ class GitlabUpdater(UpdateManager):
         }
 
     def can_handle_link(url: str):
-        return GithubUpdater.get_url_data(url) != False
+        return GitlabUpdater.get_url_data(url) != False
 
     def download(self, status_update_cb) -> str:
         target_asset = self.fetch_target_asset()
@@ -522,16 +521,19 @@ class GitlabUpdater(UpdateManager):
 
         logging.debug(f'Found {len(rel_data)} assets from {rel_url}')
 
+        if not rel_data:
+            return
+
         download_asset = None
         target_re = re.compile(self.convert_glob_to_regex(self.url_data['filename']))
 
         possible_targets = []
-        for asset in rel_data:
-            for link_data in asset['links']:
-                link_res_name = link_data['url'].split('/')[-1]
-                if re.match(target_re, link_res_name):
-                    asset['name'] = link_res_name
-                    possible_targets.append(asset)
+        asset = rel_data[0]
+        for link_data in asset['links']:
+            link_res_name = link_data['url'].split('/')[-1]
+            if re.match(target_re, link_res_name):
+                asset['name'] = link_res_name
+                possible_targets.append(asset)
 
         if len(possible_targets) == 1:
             download_asset = possible_targets[0]
@@ -542,13 +544,9 @@ class GitlabUpdater(UpdateManager):
                 logging.info(' - ' + t['name'])
 
             # Check possible differences with system architecture in file name
-            system_arch = terminal.sandbox_sh(['arch'])
-            is_x86 = re.compile(r'(\-|\_|\.)x86(\-|\_|\.)')
-            is_arm = re.compile(r'(\-|\_|\.)(arm64|aarch64|armv7l)(\-|\_|\.)')
-
-            if system_arch == 'x86_64':
+            if self.system_arch == 'x86_64':
                 for t in possible_targets:
-                    if is_x86.search(t['name']) or not is_arm.search(t['name']):
+                    if self.is_x86.search(t['name']) or not self.is_arm.search(t['name']):
                         download_asset = t
                         logging.info('found possible target: ' + t['name'])
                         break
@@ -565,6 +563,147 @@ class GitlabUpdater(UpdateManager):
 
         if target_asset:
             content_type = requests.head(target_asset['direct_asset_url']).headers.get('content-type', None)
+            ct_supported = content_type in [*AppImageProvider.supported_mimes, 'raw',
+                                                    'binary/octet-stream', 'application/octet-stream']
+
+            if ct_supported:
+                old_size = os.path.getsize(el.file_path)
+                is_size_different = target_asset['size'] != old_size
+                return is_size_different
+
+        return False
+
+class CodebergUpdater(UpdateManager):
+    staticfile_manager: Optional[StaticFileUpdater]
+    label = 'Codeberg'
+    name = 'CodebergUpdater'
+
+    def __init__(self, url) -> None:
+        super().__init__(url)
+        self.staticfile_manager = None
+        self.url_data = CodebergUpdater.get_url_data(url)
+        self.url = url
+
+        self.embedded = False
+
+    def get_url_data(url: str):
+        # Example: https://codeberg.org/sonusmix/sonusmix/releases/download/v0.1.1/org.sonusmix.Sonusmix-0.1.1.AppImage
+        paths = []
+        if url.startswith('https://'):
+            logging.debug(f'CodebergUpdater: found http url, trying to detect codeberg data')
+            urldata = urlsplit(url)
+
+            if urldata.netloc != 'codeberg.org':
+                return False
+
+            paths = urldata.path.split('/')
+
+            if len(paths) != 7:
+                return False
+
+        return {
+            'username': paths[1],
+            'repo': paths[2],
+            'filename': paths[6],
+        }
+
+    def can_handle_link(url: str):
+        return CodebergUpdater.get_url_data(url) != False
+
+    def download(self, status_update_cb) -> str:
+        target_asset = self.fetch_target_asset()
+        if not target_asset:
+            logging.warn('Missing target_asset for Codeberg instance')
+            return
+
+        dwnl = target_asset['browser_download_url']
+        self.staticfile_manager = StaticFileUpdater(dwnl)
+        fname, etag = self.staticfile_manager.download(status_update_cb)
+
+        self.staticfile_manager = None
+        return fname, target_asset['id']
+
+    def cancel_download(self):
+        if self.staticfile_manager:
+            self.staticfile_manager.cancel_download()
+            self.staticfile_manager = None
+
+    def cleanup(self):
+        if self.staticfile_manager:
+            self.staticfile_manager.cleanup()
+
+    def convert_glob_to_regex(self, glob_str):
+        """
+        Converts a string with glob patterns to a regular expression.
+
+        Args:
+            glob_str: A string containing glob patterns.
+
+        Returns:
+            A regular expression string equivalent to the glob patterns.
+        """
+        regex = ""
+        for char in glob_str:
+            if char == "*":
+                regex += r".*"
+            else:
+                regex += re.escape(char)
+
+        regex = f'^{regex}$'
+        return regex
+
+    def fetch_target_asset(self):
+        rel_url = f'https://gitlab.com/api/v4/projects/{self.url_data["username"]}/releases'
+        rel_url = f'https://codeberg.org/api/v1/repos/{self.url_data["username"]}/{self.url_data["repo"]}/releases?pre-release=exclude&draft=exclude'
+
+        try:
+            rel_data_resp = requests.get(rel_url)
+            rel_data_resp.raise_for_status()
+            rel_data = rel_data_resp.json()
+        except Exception as e:
+            logging.error(e)
+            return
+        
+
+        logging.debug(f'Found {len(rel_data)} assets from {rel_url}')
+        if not rel_data:
+            return
+
+        download_asset = None
+        target_re = re.compile(self.convert_glob_to_regex(self.url_data['filename']))
+
+        possible_targets = []
+        for asset in rel_data[0]['assets']:
+            if re.match(target_re, asset['name']):
+                possible_targets.append(asset)
+
+        if len(possible_targets) == 1:
+            download_asset = possible_targets[0]
+        else:
+            logging.info(f'found {len(possible_targets)} possible file targets')
+
+            for t in possible_targets:
+                logging.info(' - ' + t['name'])
+
+            if self.system_arch == 'x86_64':
+                for t in possible_targets:
+                    if self.is_x86.search(t['name']) or not self.is_arm.search(t['name']):
+                        download_asset = t
+                        logging.info('found possible target: ' + t['name'])
+                        break
+
+        if not download_asset:
+            logging.debug(f'No matching assets found from {rel_url}')
+            return
+
+        logging.debug(f'Found 1 matching asset: {download_asset["browser_download_url"]}')
+        return download_asset
+
+    def is_update_available(self, el: AppImageListElement):
+        target_asset = self.fetch_target_asset()
+
+        if target_asset:
+            content_type = requests.head(target_asset['browser_download_url']).headers.get('content-type', None)
             ct_supported = content_type in [*AppImageProvider.supported_mimes, 'raw',
                                                     'binary/octet-stream', 'application/octet-stream']
 
