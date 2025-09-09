09ee0eedc9361d93cb36e8383aa6d28aa250c33e
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2025-09-09 12:39:33 +0200

    Move to newer adwaita stylings

--- src/preferences.py
+++ src/preferences.py
@@ -15,7 +15,7 @@ gi.require_version('Gtk', '4.0')
 
 from gi.repository import Adw, Gtk, Gio, GLib  # noqa
 
-class Preferences(Adw.PreferencesWindow):
+class Preferences(Adw.PreferencesDialog):
     def __init__(self, **kwargs) :
         super().__init__(**kwargs)
 
