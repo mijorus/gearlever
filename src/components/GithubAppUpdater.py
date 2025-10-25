fdb60951c2ec8035d76b200c087b93723f8de0b1
Lorenzo Paderi <lorenzo@mijorus.it>
2025-10-25 12:53:20 +0200

    wip

---
+++ src/components/GithubAppUpdater.py
@@ -0,0 +1,6 @@
+from gi.repository import Gtk, Adw, GObject, Gio, Gdk
+from ..models.AppUpdaterForm import AppUpdaterForm
+
+class GithubAppUpdater(AppUpdaterForm):
+    def get_form_rows():
+        pass
