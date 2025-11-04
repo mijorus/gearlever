faa35c149105b8bdf9d00009f50487c82a79a16b
Lorenzo Paderi <lorenzo@mijorus.it>
2025-11-04 09:33:52 +0100

    deleted old file

--- src/components/GithubAppUpdater.py
+++
@@ -1,6 +0,0 @@
-from gi.repository import Gtk, Adw, GObject, Gio, Gdk
-from ..models.AppUpdaterForm import AppUpdaterForm
-
-class GithubAppUpdater(AppUpdaterForm):
-    def get_form_rows():
-        pass
