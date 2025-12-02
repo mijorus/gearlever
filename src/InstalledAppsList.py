f111346d32a7d565e1b975c1ebd086de54f04cf7
Nota Inutilis <9675966+NotaInutilis@users.noreply.github.com>
2025-12-02 12:27:10 +0100

    Revert "Ellipsis typography in original English code"
    This reverts commit 880a38f6af65e05bb478dfbb711073a8cdb10b08.

--- src/InstalledAppsList.py
+++ src/InstalledAppsList.py
@@ -28,7 +28,7 @@ class InstalledAppsList(Gtk.ScrolledWindow):
 
     CHECK_FOR_UPDATES_LABEL = _('Check for updates')
     NO_UPDATES_FOUND_LABEL = _('No updates found')
-    CHECKING_FOR_UPDATES_LABEL = _('Checking updates…')
+    CHECKING_FOR_UPDATES_LABEL = _('Checking updates...')
     UPDATE_ALL_LABEL = _('Update all')
 
     def __init__(self):
